f71505ea9f362437efd3bf8d4f96a9744cef593a
GIRISH K S <girish.ks2025aids@sece.ac.in>
2026-07-14 23:10:40 +0530

    Remove sentence-transformers to fix memory issue

--- embeddings.py
+++ embeddings.py
@@ -2,12 +2,10 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def create_vectorstore(chunks):
-    # Load HuggingFace embedding model
     embeddings = HuggingFaceEmbeddings(
-        model_name="all-MiniLM-L6-v2"
+        model_name="all-MiniLM-L6-v2",
+        model_kwargs={"device": "cpu"},
+        encode_kwargs={"normalize_embeddings": True}
     )
-
-    # Create FAISS vector store from chunks
     vectorstore = FAISS.from_documents(chunks, embeddings)
-
     return vectorstore
